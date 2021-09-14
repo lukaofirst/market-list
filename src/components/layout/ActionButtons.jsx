676462add95a730482770ab4f95835de0955415f
lukaofirst <lucasmacedo123@gmail.com>
2021-09-14 01:03:31 -0300

    refactoring add and delete function from localStorage

--- src/components/layout/ActionButtons.jsx
+++ src/components/layout/ActionButtons.jsx
@@ -2,13 +2,41 @@ import React from 'react';
 import { FaSave, FaTrash } from 'react-icons/fa';
 
 const ActionButtons = () => {
+    const addItems = () => {
+        let quantifierValues = document.querySelectorAll('#quantifier');
+
+        quantifierValues.forEach((quantifier) => {
+            saveItems(quantifier.value);
+        });
+    };
+
+    const saveItems = (item) => {
+        let items;
+        if (localStorage.getItem('buyListQuantifiers') === null) {
+            items = [];
+        } else {
+            items = JSON.parse(localStorage.getItem('buyListQuantifiers'));
+        }
+
+        items.push(item);
+
+        localStorage.setItem('buyListQuantifiers', JSON.stringify(items));
+    };
+
+    const clearSavedItems = () => {
+        if (window.confirm('Você deseja apagar os dados salvos?')) {
+            localStorage.clear();
+            window.location.reload();
+        }
+    };
+
     return (
         <div className='action-buttons'>
             <div className='container'>
-                <button className='save-changes'>
+                <button className='save-changes' onClick={addItems}>
                     <FaSave /> Salvar
                 </button>
-                <button className='delete-changes'>
+                <button className='delete-changes' onClick={clearSavedItems}>
                     <FaTrash /> Apagar
                 </button>
             </div>
