2005bd53e7e35dd2f1d2d9356f9ed165db2fdbb6
lukaofirst <lucasmacedo123@gmail.com>
2021-09-16 19:57:04 -0300

    adding checkbox on ListBoxesQuantifierSingle

--- src/components/layout/ListBoxesQuantifierSingle.jsx
+++ src/components/layout/ListBoxesQuantifierSingle.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaCheck } from 'react-icons/fa';
 
 const ListBoxesQuantifierSingle = (props) => {
+    const [takedItem, setTakedItem] = useState(false);
+
+    const toggleClassName = () => {
+        setTakedItem(!takedItem);
+    };
+
     return (
-        <div className='list-quantifier-single'>
+        <div
+            className={`list-quantifier-single ${
+                takedItem ? 'takedItem2' : ''
+            }`}
+        >
             <div className='list-quantifier-text'>
                 <p>{props.productsName}</p>
                 <span>{props.quantity}</span>
+                <div id='checkbox'>
+                    <div className='box-checkbox' onClick={toggleClassName}>
+                        <FaCheck />
+                    </div>
+                </div>
             </div>
         </div>
     );
